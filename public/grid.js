9d07808d09c7c904e0c78f003ffcb78cb92bc979
Nate <couchpotatob@gmail.com>
2019-06-02 13:34:46 -0700

    added css and some functionality to grid

--- public/grid.js
+++ public/grid.js
@@ -1,6 +1,9 @@
 function createGrid(width, height){
     
     var gridContainer = document.getElementsByClassName('grid-container')[0];
+
+    var containerWidth = gridContainer.getBoundingClientRect().width;
+    var containerHeight = gridContainer.getBoundingClientRect().height;
     
     for(var i = 0; i < height; i++){
 
@@ -8,10 +11,20 @@ function createGrid(width, height){
             var space = document.createElement('div');
             space.classList.add('grid-space');
 
+            var spaceWidth = Math.floor(containerWidth / (width + 1));
+            var spaceHeight = Math.floor(containerHeight / (height));
+
+            console.log("(" + containerWidth + ", " + containerHeight + ")" + ", (" + spaceWidth + ", " + spaceHeight + ")" );
+
+            space.setAttribute("width", spaceWidth + "px");
+            space.setAttribute("height", spaceHeight + "px");
+
             gridContainer.appendChild(space);
         }
 
     }
 }
+
+createGrid(20,20);
 
 var grid = document.getElementsByClassName('grid-container')[0];
