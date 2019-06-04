ae7f8e9a5fc369bfb8b7d3090098c93b27ad7a94
medingee <43713964+medingee@users.noreply.github.com>
2019-06-03 20:31:13 -0700

    roller update 2
    added more to the js

--- public/roller.js
+++ public/roller.js
@@ -1,12 +1,25 @@
 function rollSelection(arr){
-	var tot
+	var size = Math.floor(arr.length/2)
+	var finished = [];
 	
+	
+	for(var i = 0; i< size;i++){
+		var amount = arr.pop();
+		var side = arr.pop();
+		var sto = rollDie(side, amount);
+		
+		finished.push(sto);
+	}
+	
+	return finished
 }
 
 
 function rollDie(sides, amount){
 	var rolledArr = [];
-	
+	var identity = "faces: " + sides;
+	rolledArr.push(identity);
+	rolledArr.push(amount);
 	
 	for(var i = 0; i < amount; i++){
 		var roll = Math.random() * (sides) + 1;
