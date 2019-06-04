ff3530283b0892e2b92cc04d4bbd358156c2d06a
root <root@LAPTOP-5JTTOD06.localdomain>
2019-06-04 15:15:06 -0700

    Reads .json

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var path = require('path');
 var express = require('express');
+var exphbs = require('express-handlebars');
+var fs = require("fs");
+
+
 
 var app = express();
 var port = process.env.PORT || 3000;
+
+/*var data = fs.readFileSync("weponData.json");
+  var arrWepon = JSON.parse(data);*/
+/*app.use(express.static('public'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');*/
+
 
 
 app.use(express.static('public'));
